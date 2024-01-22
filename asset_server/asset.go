e1a2ff816054c29cc4587428c18e8e7f07d07d03
yj <20211939@edu.hanbat.ac.kr>
2024-01-22 23:39:32 +0900

    base

--- asset_server/asset.go
+++ asset_server/asset.go
@@ -4,25 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
+
 	// "go.mongodb.org/mongo-driver/mongo"
 	// "go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func setRouter(router *gin.Engine) {
+func setRouter(router *gin.Engine) { // router
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "send.html", gin.H{
-			"title": "메인페이지",
-			"url":   "/info",
+			"title": "업로드 페이지", // 제출시, 빈화면 뜸 (제출되었습니다 표시 뜨게 하기)
+			//"url":   "/info",
 		})
 	})
 
 	router.POST("/", func(c *gin.Context) {
-		fmt.Println(c.PostForm("input"))
+		fmt.Println(c.PostForm("input")) // 사용자 업로드(DB저장 필요)
 	})
 
 	router.GET("/info", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "info.html", gin.H{
-			"content": "내용",
+			"content": "내용", // '/'에서 받아온 내용 표시(DB저장 받아오기 필요)
 		})
 	})
 }
